11cacd97ac6db39608df08894acf9605fda13853
avnms <sarmaakondivnm@icloud.com>
2024-03-28 16:23:25 +1100

    Create SQLAlchemy model for notes

--- src/app/db.py
+++ src/app/db.py
@@ -1,7 +1,8 @@
 import os
 
 from databases import Database
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Table, MetaData
+from sqlalchemy.sql import func
 
 
 DATABASE_URL = os.environ("DATABASE_URL")
@@ -9,6 +10,13 @@ DATABASE_URL = os.environ("DATABASE_URL")
 # SQLAlchemy
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
+notes = Table(
+    "notes",
+    Column("id", Integer, primary_key=True),
+    Column("title", String(50)),
+    Column("description", String(50)),
+    Column("created_date", DateTime, default=func.now(), nullable=False),
+)
 
 # databases query builder
 database = Database(DATABASE_URL)
