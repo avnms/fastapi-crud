e8c4a79d6af38da3d4864b225745ef6389d5a8aa
avnms <sarmaakondivnm@icloud.com>
2024-03-28 22:04:48 +1100

    Created tests for HTTP DELETE

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -129,3 +129,33 @@ def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
         content=json.dumps(payload),
     )
     assert response.status_code == status_code
+
+
+# Test for HTTP DELETE
+def test_remove_note(test_app, monkeypatch):
+    test_data = {"id": 1, "title": "something", "description": "something else"}
+
+    async def mock_get(id):
+        return test_data
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    async def mock_delete(id):
+        return id
+
+    monkeypatch.setattr(crud, "delete", mock_delete)
+
+    response = test_app.delete("/notes/1/")
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_remove_note_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.delete("/notes/9999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Note not found"
