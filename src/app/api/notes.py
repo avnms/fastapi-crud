0cee0c32aeefb009a6c4f43601adffda0b604a45
avnms <sarmaakondivnm@icloud.com>
2024-03-28 19:35:49 +1100

    Created read_note endpoint

--- src/app/api/notes.py
+++ src/app/api/notes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from app.api import crud
 from app.api.models import NoteDB, NoteSchema
@@ -16,3 +16,11 @@ async def create_note(payload: NoteSchema):
         "description": payload.description,
     }
     return response_object
+
+
+@router.get("/{id}", response_model=NoteDB)
+async def read_note(id: int):
+    note = await crud.get(id)
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    return note
